f8c9e27e50ee71cc048348c4564861070a57005a
Ankit Jha <snssystem.ankitj@gmail.com>
2024-08-08 06:09:05 -0400

    added chnages

--- shiv_shakti/shiv_shakti/settings.py
+++ shiv_shakti/shiv_shakti/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'shiv_shakti.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shivnshakti',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # Or your MySQL server address
+        'PORT': '3306',       # Default MySQL port
     }
 }
 
